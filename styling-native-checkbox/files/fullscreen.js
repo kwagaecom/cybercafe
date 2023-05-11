2d13502aa893842ce69cf924ecacd53047f0d948
Amit Upadhyay <amupadhyay@qualys.com>
2023-05-12 00:34:06 +0530

    added fullscreen support to styling-native-checkbox app

---
+++ styling-native-checkbox/files/fullscreen.js
@@ -0,0 +1,32 @@
+
+
+/* View in fullscreen */
+function openFullscreen() {
+    var elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+}
+
+function toggleFullScreen() {
+    if (document.fullscreenElement) {
+        closeFullscreen();
+    } else {
+        openFullscreen();
+    }
+}
